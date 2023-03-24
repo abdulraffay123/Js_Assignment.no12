11154f466c63c954d2a0accd5b2c6f4f91b9c982
abdulraffay123 <abdulraffay252@gmail.com>
2023-03-25 02:20:58 +0500

    Complete

---
+++ app1.js
@@ -0,0 +1,461 @@
+// chapter 1 alert
+// alert("MADE BY ABDUL RaffaY");
+
+
+
+// var MyName = 'ABDUL RaFFaY';
+// console.log('My name is ' + MyName);
+// alert('My name is ' + MyName)
+// var myNAme = prompt("Enter Your Email") 
+// var name = ("Abdul RaffaY")
+// alert(myNAme + name)
+
+
+
+// Variable answer // 10 
+// var a = (8);
+// var b = (2);
+// alert   (a+b);
+
+
+
+// Variable answer // 1 
+// var a = (4);
+// var b = (5);
+// alert   (a-b);
+
+
+
+// Variable answer // 5 
+// var a = (1);
+// var b = (5);
+// alert   (a*b);
+
+
+
+
+// Variable answer // 15
+// var  $a = (7);
+// var  $b = (8);
+// alert   ($a+$b);
+
+
+
+
+// Variable answer // 21
+// var  _a = (5);
+// var  _b = (16);
+// alert   (_a+_b);
+
+
+
+
+// Variable answer // 28 
+// var  Ja = (11);
+// var  Kb = (17);
+// alert   (Ja+Kb);
+
+
+
+
+// Variable answer // MK 
+// var a = ("K");
+// var b = ("M");
+// var c = (b+a)
+// alert   (c);
+
+
+
+
+// Variable answer // XYZ // 22 
+// var a = (22);
+// var b = ("xyz");
+// var c = (b+a)
+// alert   (c);
+
+
+
+
+// Variable answer // 46 
+// var a = (2);
+// var b = (44);
+// var c = (a+b)
+// console.log (c)
+
+
+// Variable answer // NaN 
+// var a = (20);
+// var b = ("xyz");
+// var c = (b-a)
+// alert   (c);
+
+
+// Variable answer // 5Hellow
+// var a = (5);
+// var b = ("Hellow");
+// alert (a+b);
+
+
+
+// Variable answer // 25 
+// var a = (5);
+// var b = (5);
+
+
+
+// Variable answer // 5 
+// var a = (8);
+// var b = (3);
+// alert   (a-b);
+
+
+
+// Variable answer // 15 
+// var a = (8);
+// var b = (7);
+// alert   (a+b);
+
+
+
+// Variable answer // 85 
+// var a = (8);
+// var b = ("5");
+// alert   (a+b);
+
+
+
+// Variable answer // 10 
+// var a = ("15");
+// var b = ("5");
+// alert   (a-b);
+
+
+
+// Variable answer // NaN 
+// var a = ("World");
+// var b = ("5");
+// alert   (a-b);
+
+
+
+// Variable answer // 4 // 4 
+// var x = 3;
+// var b = ++x;
+// alert   (x);
+// alert   (b);
+
+
+
+// Variable answer // 2 // 3 
+// var x = 3;
+// var b = x--;
+// alert   (x);
+// alert   (b);
+
+
+
+// Variable answer // 3 
+// var a = 5;
+// var b = 3;
+// var c = 2;
+// var d = (a + b) - c;
+// alert (b);
+
+
+
+
+// if statements chpter 10th
+// var x = prompt("Where is country");
+// if (x === "karachi") {
+// alert("This is Correct");
+//  }
+//  else{
+//     alert("Not Correct");
+//  }
+
+
+
+// var a = prompt("Enter city Name");
+// if (a == "karachi") {
+// alert("This is Correct");
+//  }
+//  else{
+//     alert("Not Allow");
+//  }
+
+
+
+
+// f else and else if statements 12th
+//  if ( 5 === "Hellow") {
+//      alert("Correct");
+//      }
+//      else {
+//      alert("Wrong answer");
+//      }
+    
+
+
+
+// var x = prompt("What is Your age?");
+// if (x === "18") {
+//  alert("Correct");
+//  }
+//  if (x !== "18") {
+//  alert("Wrong Answer");
+//  }
+
+
+
+//  CONDITIONAL STATEMENT 
+
+// var city = prompt("Enter Your City")
+// if (city === "karachi") {
+//     alert("Clean City")
+// } else {
+//     alert("Not Clean")
+// }
+// var gender = prompt("Enter Your Gender")
+// if (gender === "male") {
+//     alert("Good Morning Sir")
+
+// } else if (gender === "female") {
+//     alert("Good morning Ma'am")
+// } else {
+//     alert("Not Found")
+// }
+
+
+
+
+// var color = prompt("SIGNAL COLOR","red yellow green")
+// if(color === "red"){
+//     alert("Must Stop")
+// }else if(color === "yellow"){
+//     alert("Ready to Move")
+// }else if(color === "green"){
+//     alert("Move Now")
+// }else{
+//     alert("CHALLAN OF YOUR BIKE ")
+// }
+
+
+
+
+// var n = 4;
+// if(++n===5){
+//     alert("true")
+// }else{
+//     alert("false")
+// }
+
+
+
+// var fuel = +prompt("Remaining fuel");
+// if(fuel < 0.25){
+//     alert('Please refill the fuel in your car')
+// }else{
+//     alert("You can drive you have fuel")
+// }
+
+
+
+// var obtained_marks = +prompt("Enter Number")
+// var totalmarks = 300;
+// var per2 = obtained_marks / totalmarks * 100;
+// alert(per2)
+
+
+
+
+// var ranging = +prompt("Enter Your Number")
+// if(ranging === 1 || ranging === 2 || ranging === 3 || ranging === 4 || ranging === 5 || ranging === 6 || ranging === 7 || ranging === 8 || ranging === 9 || ranging === 10 ){
+//     alert("Bingo! Correct Answer")
+// }else if(ranging === 1+1 || ranging === 1+1 || ranging === 3+1 || ranging === 4+1 || ranging === 5+1 || ranging === 6+1 || ranging === 7+1 || ranging === 8+1 || ranging === 9+1 || ranging === 10+1 ){
+//     alert("Close enough to the correct answer.")
+// }
+// else{
+//     alert("Your Answer in not correct.!")
+// }
+
+
+
+
+// var given_num = +prompt("Enter a number")
+// var given_num2 = given_num / 3;
+// alert(given_num2)
+
+
+
+
+// var tempreture = +prompt("Write Tempreture.!")
+// if(tempreture >= 40){
+//     alert("It is too hot outside.")
+// }else if(tempreture >= 30){
+//     alert("The Weather today is Normal.")
+// }
+// else if(tempreture >= 20){
+//     alert("Today’s Weather is cool.")
+// }
+// else if(tempreture >= 10){
+//     alert("OMG! Today’s weather is so Cool.")
+// }
+// else{
+//     alert("I Dont Know What Is Tempreture.?")
+// }
+
+
+
+// var value1 = +prompt("Enter Your Value")
+// var value2 = +prompt("Enter your second value")
+// var result1 = value1 * value2;
+// var result2 = value1 + value2
+// var result3 = value1 - value2
+// var result4 = value1 / value2
+
+
+
+
+
+
+// TESTING SET OF CONDITIONS
+
+// var digitscode = prompt("Enter Uour Alphabet")
+// var code_is = digitscode.charCodeAt();
+// document.write(digitscode + " This Alphabet code is " +  code_is + "<br>")
+// alert(code_is)
+
+
+
+
+// var num1 = +prompt("First Integar")
+// var num2 = +prompt("Second Integar")
+// if(num1 > num2){
+//     alert("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".")
+// }
+// else if(num2 > num1){
+//     alert("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".")
+// }
+// else{
+//     alert("The values "+ num1+ " and "+num2+ " are equal.")
+// }
+
+
+
+
+
+// var number = +prompt("Enter A Number","000000")
+// if(number > 0){
+//     alert("The Number Is positive")
+// }
+// else if(number === 0){
+//     alert("Number Is Zero")
+// }
+// else{
+//     alert("The Number Is Negative")
+// }
+
+
+
+
+
+// var vowel1 = prompt("Write your Vowels")
+// if(vowel1 === "a" || vowel1 === "e" || vowel1 === "i" || vowel1 === "o" || vowel1 === "u" || vowel1 === "y"){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+// alert("Length is " + vowel1.length)
+
+
+
+
+// ARRAYS //
+
+
+// var arr1 = []
+// arr1.push("RAFFAY", "AYAN","WALEED")
+// alert(arr1)
+
+
+// var arr3 = ["COW","CAMEL","HOURSE","BIRD"]
+// alert(arr3)
+
+// var arr4 = [5,7,3,9];
+// alert(arr4)
+
+// var arr5 = [true,false]
+// alert(arr5)
+
+// var arr6 = ["ABDUL",5,false,"RAFFAY",2,true]
+// alert(arr6)
+
+
+
+
+
+
+// LIST //
+
+
+// var arr10 = [320, 450, 220, 400]
+// arr10.sort()
+// document.write(arr10)
+
+// document.write("<br><br>")
+
+
+
+// // var cities = ["karachi", "lahore", "islamabad", "Quetta", "Peshawar"]
+// // var cities2 = cities.slice(2,4)
+// // document.write(cities2)
+// // document.write("<br><br>")
+
+
+
+
+//  ARRAYS AND LOOP //
+
+
+// var multiarray = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]]
+// document.write(multiarray.join("  " + "<br>"))
+//  + "<br>" + multiarray[1] + "<br>" + multiarray[2] + "<br>"  
+
+
+
+
+
+//  var tableinp = +prompt("ENTER A NUMBER TO SHOW ITS MULTIPLICATION TABLE")
+// var tableinp2 = +prompt("ENTER LENGTH MULTIPLICATION OF TABLE")
+
+
+// for (i = 1 ; i <= tableinp2; i++) {
+//     document.write( tableinp + " X " + i + " = " + tableinp * i + "<br>")
+// }
+  
+
+
+
+
+// var list_fruits = ["apple", "banana", "orange", "mango", "strawberry"]
+// for (i = 0; i < list_fruits.length; i++) {
+//     document.write("Element at Index " + i + "  " + list_fruits[i] + "<br>")
+// }
+// document.write("<br><br>")
+
+
+
+
+
+// var animals = ["Cat","Dog", "Elephant", "Wolf"]
+// var animalinp = prompt("Enter Your Animal")
+// for (p = 0; p < animals.length; p++) {
+//     if (animals[p].toUpperCase() === animalinp.toUpperCase()) {
+//         console.log(animals[p] + " Is Clean Animal")
+//         break
+//     }else{
+//         console.log(" Not clean Are Your animal ")
+        
+//     }
+// }
